18661a21c784eeacc223ecbcede59c63260df6b5
Gianni Salinetti <gbsalinetti@extraordy.com>
2018-04-28 00:08:25 +0200

    New tests

--- parameters_test.go
+++ parameters_test.go
@@ -4,8 +4,8 @@ import "testing"
 
 func TestCheckDeploymentType(t *testing.T) {
 	i := New(defaults)
-	tests := []string{"dummy", "Origin", "enter prise", "origin", ""}
-	for _, testValue := range tests {
+	badTests := []string{"dummy", "Origin", "enter prise", "origin", ""}
+	for _, testValue := range badTests {
 		i.GeneratorDeploymentType = testValue
 		err := i.CheckDeploymentType()
 		if (testValue != "origin" && testValue != "enterprise") && err == nil {
@@ -17,8 +17,8 @@ func TestCheckDeploymentType(t *testing.T) {
 func TestCheckInstallVersion(t *testing.T) {
 	i := New(defaults)
 	validVersions := []string{"v3.4", "v3.5", "v3.6", "v3.7", "v3.9", "v3.10", "v3.11"}
-	tests := []string{"v1.2", "3.9", "v3.0", "v3.6"}
-	for _, testValue := range tests {
+	badTests := []string{"v1.2", "3.9", "v3.0", "v3.6"}
+	for _, testValue := range badTests {
 		i.GeneratorInstallVersion = testValue
 		err := i.CheckInstallVersion()
 		if err == nil {
@@ -30,6 +30,58 @@ func TestCheckInstallVersion(t *testing.T) {
 				}
 			}
 			t.Error("CheckInstallVersion testing error.")
+		}
+	}
+}
+
+func TestCheckClusterMethod(t *testing.T) {
+	i := New(defaults)
+	badTests := []string{"parallel", "NATIVE", "Native", "pcs"}
+	for _, testValue := range badTests {
+		i.GeneratorClusterMethod = testValue
+		err := i.CheckClusterMethod()
+		if testValue != "native" && err == nil {
+			t.Error("CheckClusterMethod testing error.")
+		}
+	}
+}
+
+func TestCheckInfraIpv4(t *testing.T) {
+	i := New(defaults)
+	validAddr := []string{"192.168.1.20", "127.0.0.1", "172.25.250.10"}
+	badAddr := []string{"327.0.0.1", "302.200.1", "0.0,12", "a string"}
+	for _, testValue := range badAddr {
+		i.GeneratorInfraIpv4 = testValue
+		err := i.CheckInfraIpv4()
+		if err == nil {
+			for _, valid := range validAddr {
+				if valid != testValue {
+					continue
+				} else {
+					return
+				}
+			}
+			t.Error("CheckInfraIpv4 tesing error.")
+		}
+	}
+}
+
+func TestCheckSdnPlugin(t *testing.T) {
+	i := New(defaults)
+	validPlugins := []string{"ovs-subnet", "ovs-multitenant", "ovs-networkpolicy"}
+	badTests := []string{"ovs-vxlan", "dummy", "Ovs-MultiTenant", "ovs_networkpolicy"}
+	for _, testValue := range badTests {
+		i.GeneratorSdnPlugin = testValue
+		err := i.CheckSdnPlugin()
+		if err == nil {
+			for _, valid := range validPlugins {
+				if valid != testValue {
+					continue
+				} else {
+					return
+				}
+			}
+			t.Error("CheckSdnPlugin testing error.")
 		}
 	}
 }
